9535c94d4120d493a9d02266debd1062f608a424
Sebastien Castiel <sebastien@castiel.me>
2021-08-01 17:45:46 -0400

    Add README + running several programs

---
+++ README.md
@@ -0,0 +1,19 @@
+Another [Brainfuck](https://en.wikipedia.org/wiki/Brainfuck) interpreter.
+
+## Features
+
+- Supports all instructions of Brainfuck
+- Supports input and output (in the console)
+- Supports running several programs in parallel with shared memory
+
+## Utility
+
+None. I was learning Go and wanted to have fun :)
+
+## Installation
+
+`go get github.com/scastiel/go-brainfuck`
+
+## Usage
+
+`go-brainfuck examples/hello.bf`
--- main.go
+++ main.go
@@ -5,21 +5,30 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"sync"
 
 	brainfuck "github.com/scastiel/go-brainfuck/lib/brainfuck"
 	memory "github.com/scastiel/go-brainfuck/lib/memory"
 )
 
 func main() {
-	code, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		log.Fatal("Can't read input file")
-	}
-
 	memory := memory.NewMemory()
 	out := bufio.NewWriterSize(os.Stdout, 1)
 	in := bufio.NewReaderSize(os.Stdin, 1)
 
-	brainfuck.Execute(string(code), 0, &memory, out, in)
+	var wg sync.WaitGroup
+	for i, file := range os.Args[1:] {
+		wg.Add(1)
+		go func(i int) {
+			code, err := ioutil.ReadFile(file)
+			if err != nil {
+				log.Fatal("Can't read input file")
+			}
+			brainfuck.Execute(string(code), i*1000, &memory, out, in)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
 	out.Flush()
 }
